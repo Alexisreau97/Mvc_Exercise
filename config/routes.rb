d49a9492486b4e947610fab54848505e83b11ef6
Alexisreau97 <alexis.reaupro@gmail.com>
2019-01-14 12:57:53 +0100

    add user authentication systeme

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   root 'administration/items#index'
 
   get '/home', to: 'home#landing_page'
 
   namespace 'administration' do
     get '/', to: 'items#index'
-
     resources :items
   end
 end
