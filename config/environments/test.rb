bdb5c36ff166f69f734774197f6e1691a5d62df7
Alexisreau97 <alexis.reaupro@gmail.com>
2019-01-15 16:55:54 +0100

    try again to config maildev

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
 
   # Mailer
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+
   config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
         :address => "localhost",
