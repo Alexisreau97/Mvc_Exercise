ea4d9096e09b3ed330f6824dbb305e22a9acb43b
Alexisreau97 <alexis.reaupro@gmail.com>
2019-01-16 14:39:42 +0100

    add sidekiq notification

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_client do |config|
-	config.redis = { url: Rails.application.credentials[:REDIS_URL] }
+  config.redis = { url: Rails.application.credentials[:REDIS_URL] }
 end
 
 Sidekiq.configure_server do |config|
-	config.redis = { url: Rails.application.credentials[:REDIS_URL] }
+  config.redis = { url: Rails.application.credentials[:REDIS_URL] }
 end
