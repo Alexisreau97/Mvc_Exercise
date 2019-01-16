4a134c34f4c8ff9ee008c88fd38d0ec72ba1b9e2
Alexisreau97 <alexis.reaupro@gmail.com>
2019-01-16 13:30:57 +0100

    sidekiq config sucess

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,7 @@
+Sidekiq.configure_client do |config|
+	config.redis = { url: Rails.application.credentials[:REDIS_URL] }
+end
+
+Sidekiq.configure_server do |config|
+	config.redis = { url: Rails.application.credentials[:REDIS_URL] }
+end
